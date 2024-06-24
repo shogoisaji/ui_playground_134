48924bb0e112ae73e71c529de7cf3ffa0b7259d8
shogoisaji <simacsimac9@gmail.com>
2024-06-24 18:51:47 +0900

    feat: particle hole

--- lib/pages/particle_hole/particle_hole_expample.dart
+++ lib/pages/particle_hole/particle_hole_expample.dart
@@ -15,67 +15,75 @@ class ParticleHoleExample extends StatefulWidget {
 
 class _ParticleHoleExampleState extends State<ParticleHoleExample>
     with TickerProviderStateMixin {
-  static const double _initialRadius = 20;
+  static const double _initialHoleRadius = 20;
 
-  late AnimationController _startAnimationController;
-  late Animation<double> _startAnimation;
-  late AnimationController _circleMoveAnimationController;
-  late Animation<double> _circleMoveAnimation;
-  final List<Particle> particles = [];
+  late AnimationController _appearAnimationController;
+  late Animation<double> _appearAnimation;
+  late AnimationController _holeScaleAnimationController;
+  late Animation<double> _holeScaleAnimation;
   late Ticker _ticker;
-  double _opacity = 1.0;
-  double _circleRadius = _initialRadius;
+  late double _holeRadius;
+
+  final List<Particle> particles = [];
+
   double _particleRadius = 0;
   Offset position = const Offset(0, 0);
-  Offset _clipCenterPosition = const Offset(0, 0);
+
+  Offset _calcHoleCenterPosition(
+      double width, double height, double animationValue) {
+    return Offset(width / 2, height / 2 * animationValue);
+  }
+
   @override
   void initState() {
     super.initState();
 
-    _startAnimationController = AnimationController(
+    _holeRadius = _initialHoleRadius;
+
+    _appearAnimationController = AnimationController(
         vsync: this, duration: const Duration(milliseconds: 3000));
-    _startAnimation = CurvedAnimation(
-        parent: _startAnimationController, curve: Curves.easeOutQuart);
+    _appearAnimation = CurvedAnimation(
+        parent: _appearAnimationController, curve: Curves.easeOutQuart);
+
+    /// initial animation
     Future.delayed(const Duration(milliseconds: 500), () {
-      _startAnimationController.forward();
+      _appearAnimationController.forward();
     });
 
-    _circleMoveAnimationController = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 3000));
-    _circleMoveAnimation = CurvedAnimation(
-        parent: _circleMoveAnimationController, curve: Curves.easeInOut);
+    _holeScaleAnimationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 2000));
+    _holeScaleAnimation = CurvedAnimation(
+        parent: _holeScaleAnimationController, curve: Curves.easeInOut);
 
-    _ticker = createTicker((elapsed) {
+    _ticker = createTicker((_) {
       for (var i = 0; i < 30; i++) {
-        /// 1270個のパーティクルを生成 127個/i
-        particles.add(Particle(particleRadius: _particleRadius));
+        particles.add(Particle(holeRadius: _particleRadius));
       }
       for (var particle in particles) {
         particle.move();
       }
 
-      /// opacityが0になったパーティクルを削除
-      particles.removeWhere((particle) => particle.opacity <= 0);
+      /// particle opacityが0になったパーティクルを削除
+      particles.removeWhere((particle) => particle.particleRadius <= 0);
       setState(() {});
     });
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
       final size = MediaQuery.sizeOf(context);
-      // position = Offset(size.width / 2, size.height / 2);
-      _circleMoveAnimation.addListener(() {
+      _holeScaleAnimation.addListener(() {
+        const initialRadius = 2.5;
+        const scaleRate = 6;
         setState(() {
-          _particleRadius = _circleMoveAnimation.value * 6 + 2.5;
-          _opacity = (_circleMoveAnimation.value + 0.5).clamp(0, 1);
-          _circleRadius = size.width > size.height
-              ? (size.width + 100) / 2 * _circleMoveAnimation.value +
-                  _initialRadius
-              : (size.height + 100) / 2 * _circleMoveAnimation.value +
-                  _initialRadius;
-        });
-      });
-      _startAnimation.addListener(() {
-        setState(() {
-          _clipCenterPosition = Offset(MediaQuery.sizeOf(context).width / 2,
-              (MediaQuery.sizeOf(context).height / 2 * _startAnimation.value));
+          /// particle radius update
+          _particleRadius =
+              _holeScaleAnimation.value * scaleRate + initialRadius;
+
+          /// hole radius update
+          _holeRadius = size.width > size.height
+              ? (size.width + 100) / 2 * _holeScaleAnimation.value +
+                  _initialHoleRadius
+              : (size.height + 100) / 2 * _holeScaleAnimation.value +
+                  _initialHoleRadius;
         });
       });
     });
@@ -83,8 +91,8 @@ class _ParticleHoleExampleState extends State<ParticleHoleExample>
 
   @override
   void dispose() {
-    _circleMoveAnimationController.dispose();
-    _startAnimationController.dispose();
+    _holeScaleAnimationController.dispose();
+    _appearAnimationController.dispose();
     _ticker.dispose();
     super.dispose();
   }
@@ -96,17 +104,15 @@ class _ParticleHoleExampleState extends State<ParticleHoleExample>
         backgroundColor: Colors.grey.shade100,
         actions: [
           IconButton(
-            icon: const Icon(Icons.replay_outlined, color: Colors.white),
+            icon: const Icon(Icons.replay_outlined),
             onPressed: () {
-              setState(() {
-                particles.clear();
-                _ticker.stop();
-                _startAnimationController.reset();
-                _circleMoveAnimationController.reset();
-                _circleRadius = _initialRadius;
-                Future.delayed(const Duration(milliseconds: 500), () {
-                  _startAnimationController.forward();
-                });
+              particles.clear();
+              _ticker.stop();
+              _appearAnimationController.reset();
+              _holeScaleAnimationController.reset();
+              _holeRadius = _initialHoleRadius;
+              Future.delayed(const Duration(milliseconds: 500), () {
+                _appearAnimationController.forward();
               });
             },
           ),
@@ -117,98 +123,121 @@ class _ParticleHoleExampleState extends State<ParticleHoleExample>
               })
         ],
       ),
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Opacity(
-            opacity: _opacity,
-            child: CustomPaint(
-              painter: ParticlePainter(
-                centerPosition: _clipCenterPosition,
-                particles: particles,
-                circleRadius: _circleRadius,
+      body: AnimatedBuilder(
+        animation: _appearAnimation,
+        builder: (context, child) =>
+            LayoutBuilder(builder: (context, constraints) {
+          final holeCenterPosition = _calcHoleCenterPosition(
+              constraints.maxWidth,
+              constraints.maxHeight,
+              _appearAnimation.value);
+          return Stack(
+            fit: StackFit.expand,
+            children: [
+              _buildParticle(holeCenterPosition),
+              _buildHole(holeCenterPosition),
+              _buildPlayButton(),
+            ],
+          );
+        }),
+      ),
+    );
+  }
+
+  Widget _buildParticle(Offset holeCenterPosition) {
+    return CustomPaint(
+      painter: ParticlePainter(
+        centerPosition: holeCenterPosition,
+        particles: particles,
+        holeRadius: _holeRadius,
+        holeScaleAnimationValue: _holeScaleAnimation.value,
+      ),
+    );
+  }
+
+  Widget _buildHole(Offset holeCenterPosition) {
+    return ClipPath(
+      clipper:
+          HoleClipper(centerPosition: holeCenterPosition, radius: _holeRadius),
+      child: Container(
+        color: Colors.grey[900],
+        width: _holeRadius,
+        height: _holeRadius,
+      ),
+    );
+  }
+
+  Widget _buildPlayButton() {
+    return Opacity(
+      opacity: (1 - _holeScaleAnimation.value * 3).clamp(0, 1),
+      child: Align(
+          alignment: const Alignment(0, 0.5),
+          child: GestureDetector(
+            onTap: () {
+              if (_ticker.isTicking) {
+                _ticker.stop();
+                _holeScaleAnimationController.stop();
+              } else {
+                _ticker.start();
+                _holeScaleAnimationController.forward();
+              }
+            },
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white, width: 2),
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                _ticker.isTicking ? Icons.pause : Icons.play_arrow,
+                color: Colors.white,
+                size: 40,
               ),
             ),
-          ),
-          ClipPath(
-            clipper: MyClipper(
-                centerPosition: _clipCenterPosition, radius: _circleRadius),
-            child: Container(
-              color: Colors.grey[900],
-              width: _circleRadius,
-              height: _circleRadius,
-            ),
-          ),
-          Opacity(
-            opacity: (1 - _circleMoveAnimation.value * 3).clamp(0, 1),
-            child: Align(
-                alignment: const Alignment(0, 0.5),
-                child: GestureDetector(
-                  onTap: () {
-                    if (_ticker.isTicking) {
-                      _ticker.stop();
-                      _circleMoveAnimationController.stop();
-                    } else {
-                      _ticker.start();
-                      _circleMoveAnimationController.forward();
-                    }
-                  },
-                  child: Container(
-                    width: 60,
-                    height: 60,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white, width: 2),
-                      shape: BoxShape.circle,
-                    ),
-                    padding: const EdgeInsets.all(4),
-                    child: const Icon(
-                      Icons.play_arrow,
-                      color: Colors.white,
-                      size: 40,
-                    ),
-                  ),
-                )),
-          ),
-        ],
-      ),
+          )),
     );
   }
 }
 
 class Particle {
-  double angle = Random().nextDouble() * 2 * pi;
+  Particle({required this.holeRadius});
+  double holeRadius;
+
+  final double angle = Random().nextDouble() * 2 * pi;
+
   double x = 0;
   double y = 0;
-  double particleRadius;
   int opacity = 255;
-
-  Particle({required this.particleRadius});
+  double particleRadius = 3;
 
   void move() {
-    final vx = cos(angle) * particleRadius / 3;
-    final vy = sin(angle) * particleRadius / 3;
+    final vx = cos(angle) * 1.5;
+    final vy = sin(angle) * 1.5;
+
     x -= vx;
     y -= vy;
-    particleRadius -= 0.05;
+    particleRadius -= 0.02;
     opacity -= 2;
   }
 }
 
 class ParticlePainter extends CustomPainter {
-  final Offset centerPosition;
-  final List<Particle> particles;
-  final double circleRadius;
   ParticlePainter(
       {required this.particles,
       required this.centerPosition,
-      required this.circleRadius});
+      required this.holeRadius,
+      required this.holeScaleAnimationValue});
+  final List<Particle> particles;
+  final Offset centerPosition;
+  final double holeRadius;
+  final double holeScaleAnimationValue;
 
   @override
   void paint(Canvas canvas, Size size) {
-    Offset calculatePosition(Particle particle) {
+    Offset calcPosition(Particle particle) {
       return Offset(
-          centerPosition.dx + cos(particle.angle) * circleRadius + particle.x,
-          centerPosition.dy + sin(particle.angle) * circleRadius + particle.y);
+          centerPosition.dx + cos(particle.angle) * holeRadius + particle.x,
+          centerPosition.dy + sin(particle.angle) * holeRadius + particle.y);
     }
 
     for (var particle in particles) {
@@ -217,8 +246,8 @@ class ParticlePainter extends CustomPainter {
             Colors.grey[800]!.withOpacity((particle.opacity / 255).clamp(0, 1))
         ..style = PaintingStyle.fill;
 
-      canvas.drawCircle(
-          calculatePosition(particle), particle.particleRadius, paint);
+      canvas.drawCircle(calcPosition(particle),
+          particle.particleRadius * holeScaleAnimationValue * 3, paint);
     }
   }
 
@@ -226,10 +255,10 @@ class ParticlePainter extends CustomPainter {
   bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
 
-class MyClipper extends CustomClipper<Path> {
+class HoleClipper extends CustomClipper<Path> {
   final Offset centerPosition;
   final double radius;
-  MyClipper({required this.centerPosition, required this.radius});
+  HoleClipper({required this.centerPosition, required this.radius});
   @override
   Path getClip(Size size) {
     return Path()
--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ui_playground_134/pages/follow_path/follow_path_example.dart';
+import 'package:ui_playground_134/pages/particle_hole/particle_hole_expample.dart';
 
 import 'package:ui_playground_134/pages/stripe_animation/stripe_animation_example.dart';
 import 'package:ui_playground_134/strings.dart';
 
 // 新しいpageを追加する場合は、pagesリストにMapを追加する
 List<Map<String, dynamic>> pages = [
+  {
+    'name': 'particle_hole',
+    'page': const ParticleHoleExample(
+      githubUrl:
+          'https://github.com/shogoisaji/ui_playground_134/blob/main/lib/pages/particle_hole/particle_hole_expample.dart',
+    ),
+    'date': DateTime(2024, 6, 24),
+    'thumbnail': 'particle_hole.gif',
+  },
   {
     'name': 'follow_path',
     'page': const FollowPathExample(
