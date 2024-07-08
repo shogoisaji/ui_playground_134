f92243e023c0ce77ba3cb3492321c35d7195460f
shogoisaji <simacsimac9@gmail.com>
2024-07-08 17:45:27 +0900

    update: home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,6 +17,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool _isIos = false;
+  int? _hoverIndex;
+
   void detectDevice() {
     String userAgent = html.window.navigator.userAgent.toLowerCase();
     if (userAgent.contains('iphone') || userAgent.contains('ipad')) {
@@ -61,8 +63,22 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Stack(
         children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: const Alignment(-0.5, 0.0),
+                end: const Alignment(0.0, 0.9),
+                colors: [
+                  const Color(0xFF01579B),
+                  Colors.blue.shade200,
+                ],
+              ),
+            ),
+          ),
           Align(
-            alignment: const Alignment(0.9, 0.9),
+            alignment: const Alignment(0.9, 0.95),
             child: Opacity(
               opacity: 0.5,
               child: Image.asset(
@@ -72,123 +88,129 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-          Center(
-            child: Column(
-              children: [
-                Expanded(
-                  child: Container(
-                    constraints: const BoxConstraints(maxWidth: 1000),
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithMaxCrossAxisExtent(
-                        maxCrossAxisExtent: 300,
-                        childAspectRatio: 1.0,
-                      ),
-                      itemCount: pages.length + 2,
-                      itemBuilder: (context, index) => index > pages.length - 1
-                          ? const SizedBox.shrink()
-                          : Card(
-                              margin: const EdgeInsets.all(8),
-                              elevation: 6,
-                              color: const Color(0xFF54C5F8),
-                              child: InkWell(
-                                onTap: () {
-                                  context.go('/${pages[index]['name']}',
-                                      extra: pages[index]['github']);
-                                },
-                                child: Column(
-                                  children: [
-                                    Center(
-                                      child: Padding(
-                                        padding: const EdgeInsets.only(
-                                            top: 3.0,
-                                            bottom: 0.0,
-                                            left: 4.0,
-                                            right: 4.0),
-                                        child: Text(
-                                          '${pages[index]['name'].split('_').map((word) {
-                                            final String w = word;
-                                            return w.capitalize();
-                                          }).join(' ')}',
-                                          overflow: TextOverflow.ellipsis,
-                                          style: TextStyle(
-                                              color: Colors.grey[800],
-                                              fontSize:
-                                                  w > breakpoint ? 24 : 20,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                      ),
-                                    ),
-                                    Expanded(
-                                      child: Container(
-                                        width: double.infinity,
-                                        padding: const EdgeInsets.only(
-                                            top: 2,
-                                            bottom: 6,
-                                            left: 6,
-                                            right: 6),
-                                        margin: const EdgeInsets.symmetric(
-                                            vertical: 5.0, horizontal: 6.0),
-                                        decoration: BoxDecoration(
-                                          color: Colors.blue.shade50,
-                                          borderRadius:
-                                              BorderRadius.circular(8),
-                                          boxShadow: [
-                                            BoxShadow(
-                                              color:
-                                                  Colors.black.withOpacity(0.3),
-                                              blurRadius: 3,
-                                              offset: const Offset(0, 1),
-                                            ),
-                                          ],
-                                        ),
-                                        child: Column(
-                                          children: [
-                                            Text(
-                                              (pages[index]['date'] as DateTime)
-                                                  .toIso8601String()
-                                                  .toYMDString(),
-                                              overflow: TextOverflow.ellipsis,
-                                              style: TextStyle(
-                                                color: Colors.grey[500],
-                                                fontSize: 14,
-                                              ),
-                                            ),
-                                            Expanded(
-                                              child: ClipRRect(
-                                                borderRadius:
-                                                    BorderRadius.circular(6),
-                                                child: RepaintBoundary(
-                                                  child: SizedBox(
-                                                    width: double.infinity,
-                                                    child: _isIos
-                                                        ? _errorImage()
-                                                        : Image.asset(
-                                                            'assets/thumbnails/${pages[index]['thumbnail']}',
-                                                            fit: BoxFit.cover,
-                                                            errorBuilder:
-                                                                (context, error,
-                                                                    stack) {
-                                                              return _errorImage();
-                                                            },
-                                                          ),
-                                                  ),
-                                                ),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  ],
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            constraints: const BoxConstraints(maxWidth: 1000),
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 300,
+                childAspectRatio: 1.0,
+              ),
+              itemCount: pages.length + 2,
+              itemBuilder: (context, index) => index > pages.length - 1
+                  ? const SizedBox.shrink()
+                  : Card(
+                      margin: const EdgeInsets.only(
+                          top: 16, bottom: 0, left: 8, right: 8),
+                      elevation: 6,
+                      color: const Color(0xFF54C5F8),
+                      child: MouseRegion(
+                        onHover: (_) {
+                          setState(() {
+                            _hoverIndex = index;
+                          });
+                        },
+                        onExit: (_) {
+                          setState(() {
+                            _hoverIndex = null;
+                          });
+                        },
+                        child: GestureDetector(
+                          onTap: () {
+                            context.go('/${pages[index]['name']}',
+                                extra: pages[index]['github']);
+                          },
+                          child: Column(
+                            children: [
+                              Center(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 3.0,
+                                      bottom: 0.0,
+                                      left: 4.0,
+                                      right: 4.0),
+                                  child: Text(
+                                    '${pages[index]['name'].split('_').map((word) {
+                                      final String w = word;
+                                      return w.capitalize();
+                                    }).join(' ')}',
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                        color: Colors.grey[800],
+                                        fontSize: w > breakpoint ? 24 : 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                 ),
                               ),
-                            ),
+                              Expanded(
+                                child: Container(
+                                  width: double.infinity,
+                                  padding: const EdgeInsets.only(
+                                      top: 2, bottom: 6, left: 6, right: 6),
+                                  margin: const EdgeInsets.symmetric(
+                                      vertical: 5.0, horizontal: 6.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.blue.shade50,
+                                    borderRadius: BorderRadius.circular(8),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black.withOpacity(0.3),
+                                        blurRadius: 3,
+                                        offset: const Offset(0, 1),
+                                      ),
+                                    ],
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        (pages[index]['date'] as DateTime)
+                                            .toIso8601String()
+                                            .toYMDString(),
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                          color: Colors.grey[500],
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(6),
+                                          child: RepaintBoundary(
+                                            child: SizedBox(
+                                              width: double.infinity,
+                                              child: index == _hoverIndex
+                                                  ? Image.asset(
+                                                      'assets/animated_thumbnails/${pages[index]['name']}.webp',
+                                                      fit: BoxFit.cover,
+                                                      errorBuilder: (context,
+                                                          error, stack) {
+                                                        return _errorImage();
+                                                      },
+                                                    )
+                                                  : Image.asset(
+                                                      'assets/thumbnails/${pages[index]['name']}.png',
+                                                      fit: BoxFit.cover,
+                                                      errorBuilder: (context,
+                                                          error, stack) {
+                                                        return _errorImage();
+                                                      },
+                                                    ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              ],
             ),
           ),
         ],
