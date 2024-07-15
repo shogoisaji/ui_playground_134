59a82d09a0b797e561d04b233b80bdd2ae5aed80
shogoisaji <shogoisaji@users.noreply.github.com>
2024-07-15 11:55:37 +0000

    deploy: a36190744b99d7fae755179242e8fbaaca569f73

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,7 +19,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "canvaskit/skwasm.js": "5d4f9263ec93efeb022bb14a3881d240",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "ede3d6e2cc0518bf69f9d9834b4c47df",
+"flutter_bootstrap.js": "e3a1982a23d25796116672704dc89f5c",
 "version.json": "5112423804a8cd2371214e0cbc39e6c8",
 "favicon.png": "3960b7140381a3eadab320ac9f629cd7",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "f3307f62ddff94d2cd8b103daf8d1b0f",
@@ -67,8 +67,8 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "assets/fonts/MaterialIcons-Regular.otf": "2de0f15d54ac406c6aa61aeb813ffe7b",
 "assets/AssetManifest.json": "a4c8e4547e7f9d8a3d507d5b5b7bb7cb",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
-"index.html": "8584ee1311f5ebcbcb32ea688950cba4",
-"/": "8584ee1311f5ebcbcb32ea688950cba4",
+"index.html": "3cca0e9367b2e97f795ad06766ea68bf",
+"/": "3cca0e9367b2e97f795ad06766ea68bf",
 "main.dart.js": "e478ce3df7b4761ba48bc17087eaa943"};
 // The application shell files that are downloaded before a service worker can
 // start.
