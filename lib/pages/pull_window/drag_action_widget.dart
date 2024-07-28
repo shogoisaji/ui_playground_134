8b34832b8c4ea7059e074dbe88b91576166b51a6
shogoisaji <simacsimac9@gmail.com>
2024-07-28 20:37:39 +0900

    update: add overlay

--- lib/pages/pull_window/drag_action_widget.dart
+++ lib/pages/pull_window/drag_action_widget.dart
@@ -35,15 +35,18 @@ class _DragActionWidgetState extends State<DragActionWidget>
   late AnimationController controller;
   late Animation<Offset> positionAnimation;
 
-  double distance = 0;
-  double angle = 0.0;
-  double initialDistance = 0;
+  OverlayEntry? _overlay;
 
-  bool isIntoArea = false;
+  double _distance = 0;
+  double _angle = 0.0;
+  double _initialDistance = 0;
 
-  late Offset position;
+  bool _isIntoArea = false;
+  bool _isDragging = false;
 
-  void setAnimation(Offset start, Offset end) {
+  late Offset _position;
+
+  void _setAnimation(Offset start, Offset end) {
     positionAnimation = Tween<Offset>(begin: start, end: end).animate(
       CurvedAnimation(
         parent: controller,
@@ -52,54 +55,120 @@ class _DragActionWidgetState extends State<DragActionWidget>
     );
   }
 
-  void updateAngle() {
-    final nextAngle = widget.initialAngle * (distance / initialDistance);
+  void _updateAngle() {
+    final nextAngle = widget.initialAngle * (_distance / _initialDistance);
     setState(() {
       if (widget.initialAngle > 0) {
-        angle = nextAngle.clamp(0, widget.initialAngle);
+        _angle = nextAngle.clamp(0, widget.initialAngle);
       } else {
-        angle = nextAngle.clamp(widget.initialAngle, 0);
+        _angle = nextAngle.clamp(widget.initialAngle, 0);
       }
     });
-    if (distance < widget.intoArea) {
+    if (_distance < widget.intoArea) {
       setState(() {
-        isIntoArea = true;
+        _isIntoArea = true;
       });
     } else {
       setState(() {
-        isIntoArea = false;
+        _isIntoArea = false;
       });
     }
   }
 
-  void backInitialPosition() {
-    setAnimation(
-      position,
+  void _backInitialPosition() {
+    _setAnimation(
+      _position,
       widget.initialPosition,
     );
     controller.reset();
     controller.forward();
   }
 
+  void _handlePanStart() {
+    setState(() {
+      _isDragging = true;
+    });
+    final double topPadding = MediaQuery.of(context).padding.top;
+    final double appBarHeight = AppBar().preferredSize.height;
+    _overlay = OverlayEntry(
+      builder: (context) {
+        return AnimatedBuilder(
+          animation: positionAnimation,
+          builder: (context, child) {
+            return Positioned(
+              top: _position.dy -
+                  widget.height / 2 +
+                  (topPadding + appBarHeight),
+              left: _position.dx - widget.width / 2,
+              child: Transform.rotate(
+                angle: _angle,
+                child: Material(
+                  color: Colors.transparent,
+                  child: _cardWidget(
+                    _isIntoArea ? widget.appliedWidget : widget.initialWidget,
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+    Overlay.of(context).insert(_overlay!);
+  }
+
+  void _dragEnd() {
+    _overlay?.remove();
+    _overlay = null;
+  }
+
+  void _handlePanUpdate(DragUpdateDetails details) {
+    setState(() {
+      _position = Offset(
+        _position.dx + details.delta.dx,
+        _position.dy + details.delta.dy,
+      );
+      _distance = (widget.targetPosition - _position).distance;
+    });
+    _updateAngle();
+    _overlay?.markNeedsBuild();
+  }
+
+  void _handlePanEnd(DragEndDetails details) async {
+    if (_isIntoArea) {
+      widget.onAccepted();
+      _setAnimation(_position, widget.targetPosition);
+    } else {
+      _setAnimation(_position, widget.initialPosition);
+    }
+    controller.reset();
+    await controller.forward();
+    _dragEnd();
+    setState(() {
+      _isDragging = false;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
-    position = widget.initialPosition;
+    _position = widget.initialPosition;
     controller = AnimationController(
       duration: const Duration(milliseconds: 500),
       vsync: this,
     );
     controller.addListener(() {
       setState(() {
-        position = positionAnimation.value;
+        _position = positionAnimation.value;
       });
-      distance = (widget.targetPosition - position).distance;
-      updateAngle();
+      _distance = (widget.targetPosition - _position).distance;
+      _updateAngle();
     });
-    initialDistance = (widget.targetPosition - widget.initialPosition).distance;
-    distance = (widget.targetPosition - position).distance;
-    setAnimation(widget.initialPosition, widget.targetPosition);
-    updateAngle();
+    _initialDistance =
+        (widget.targetPosition - widget.initialPosition).distance;
+    _distance = (widget.targetPosition - _position).distance;
+    _setAnimation(widget.initialPosition, widget.targetPosition);
+    _updateAngle();
   }
 
   @override
@@ -112,7 +181,7 @@ class _DragActionWidgetState extends State<DragActionWidget>
   void didUpdateWidget(DragActionWidget oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (widget.isAccepted != oldWidget.isAccepted && !widget.isAccepted) {
-      backInitialPosition();
+      _backInitialPosition();
     }
   }
 
@@ -120,42 +189,31 @@ class _DragActionWidgetState extends State<DragActionWidget>
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        AnimatedBuilder(
-          animation: positionAnimation,
-          builder: (context, child) {
-            return Positioned(
-              top: position.dy - widget.height / 2,
-              left: position.dx - widget.width / 2,
-              child: GestureDetector(
-                onPanUpdate: (details) {
-                  setState(() {
-                    position = Offset(
-                      position.dx + details.delta.dx,
-                      position.dy + details.delta.dy,
-                    );
-                    distance = (widget.targetPosition - position).distance;
-                  });
-                  updateAngle();
-                },
-                onPanEnd: (details) {
-                  if (isIntoArea) {
-                    widget.onAccepted();
-                    setAnimation(position, widget.targetPosition);
-                  } else {
-                    setAnimation(position, widget.initialPosition);
-                  }
-                  controller.reset();
-                  controller.forward();
-                },
-                child: Transform.rotate(
-                  angle: angle,
-                  child: _cardWidget(
-                    isIntoArea ? widget.appliedWidget : widget.initialWidget,
-                  ),
+        Positioned(
+          top: _position.dy - widget.height / 2,
+          left: _position.dx - widget.width / 2,
+          child: GestureDetector(
+            onPanStart: (details) {
+              _handlePanStart();
+            },
+            onPanUpdate: (details) {
+              _handlePanUpdate(details);
+            },
+            onPanEnd: (details) {
+              _handlePanEnd(details);
+            },
+            child: Transform.rotate(
+              angle: _angle,
+              child: Opacity(
+                opacity: _isDragging ? 0 : 1,
+                child: _cardWidget(
+                  widget.isAccepted
+                      ? widget.appliedWidget
+                      : widget.initialWidget,
                 ),
               ),
-            );
-          },
+            ),
+          ),
         ),
       ],
     );
