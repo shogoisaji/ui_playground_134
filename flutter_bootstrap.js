1adb4e0d247ceb0c16613a6642f48933e96c7533
shogoisaji <shogoisaji@users.noreply.github.com>
2024-07-28 11:58:33 +0000

    deploy: a447a72cdcf5f10016e3fdfdee92056f52ea7040

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1598618575"
+    serviceWorkerVersion: "1391655511"
   }
 });
